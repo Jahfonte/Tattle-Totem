0c5f81b3691810a4ba5723190e73e83ee48d75ac
Jahfonte <jahbulon@duck.com>
2025-12-23 11:11:57 -0800

    Add TattleTotem addon files directly

---
+++ TattleTotem/SpellData.lua
@@ -0,0 +1,49 @@
+TattleTotem_AoESpells = {
+    ["Multi-Shot"] = { class = "HUNTER", type = "ranged_aoe" },
+    ["Volley"] = { class = "HUNTER", type = "ranged_aoe" },
+    ["Carve"] = { class = "HUNTER", type = "melee_aoe" },
+    ["Explosive Trap"] = { class = "HUNTER", type = "trap_aoe" },
+
+    ["Arcane Explosion"] = { class = "MAGE", type = "pbaoe" },
+    ["Blizzard"] = { class = "MAGE", type = "channeled_aoe" },
+    ["Cone of Cold"] = { class = "MAGE", type = "cone_aoe" },
+    ["Flamestrike"] = { class = "MAGE", type = "ground_aoe" },
+    ["Blast Wave"] = { class = "MAGE", type = "pbaoe" },
+    ["Frost Nova"] = { class = "MAGE", type = "pbaoe" },
+    ["Icicles"] = { class = "MAGE", type = "channeled_aoe" },
+
+    ["Rain of Fire"] = { class = "WARLOCK", type = "channeled_aoe" },
+    ["Hellfire"] = { class = "WARLOCK", type = "channeled_pbaoe" },
+    ["Howl of Terror"] = { class = "WARLOCK", type = "pbaoe_fear" },
+
+    ["Holy Wrath"] = { class = "PALADIN", type = "pbaoe" },
+    ["Consecration"] = { class = "PALADIN", type = "ground_aoe" },
+
+    ["Chain Lightning"] = { class = "SHAMAN", type = "chain" },
+    ["Magma Totem"] = { class = "SHAMAN", type = "totem_aoe" },
+    ["Fire Nova Totem"] = { class = "SHAMAN", type = "totem_aoe" },
+
+    ["Holy Nova"] = { class = "PRIEST", type = "pbaoe" },
+
+    ["Hurricane"] = { class = "DRUID", type = "channeled_aoe" },
+    ["Swipe"] = { class = "DRUID", type = "melee_aoe" },
+
+    ["Whirlwind"] = { class = "WARRIOR", type = "melee_aoe" },
+    ["Cleave"] = { class = "WARRIOR", type = "melee_cleave" },
+    ["Thunder Clap"] = { class = "WARRIOR", type = "pbaoe" },
+    ["Sweeping Strikes"] = { class = "WARRIOR", type = "buff_cleave" },
+
+    ["Blade Flurry"] = { class = "ROGUE", type = "buff_cleave" },
+}
+
+TattleTotem_FourHorsemen = {
+    ["Thane Korth'azz"] = true,
+    ["Highlord Mograine"] = true,
+    ["Sir Zeliek"] = true,
+    ["Lady Blaumeux"] = true,
+}
+
+TattleTotem_KTMobs = {
+    ["Kel'Thuzad"] = true,
+    ["Guardian of Icecrown"] = true,
+}
